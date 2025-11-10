625f8d474dd3c38c493067df93e2838b63c3e5ac
vagnereix <vagnereix@users.noreply.github.com>
2025-11-10 12:43:28 +0000

    style: Update design tokens

---
+++ build/js/tokens-tailwind-utilities.js
@@ -0,0 +1,184 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 10 Nov 2025 12:43:27 GMT
+ */
+export const tokensUtilities = {
+  '.grid-web-grid0': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(16, minmax(0, 1fr))',
+    columnGap: '1rem',
+    paddingInline: '1.5rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-web-grid1': {
+    display: 'grid',
+    gridTemplateRows: 'repeat(10, minmax(0, 1fr))',
+    rowGap: '1rem',
+    paddingInline: '1.5rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-web-vertical': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(16, minmax(0, 1fr))',
+    columnGap: '1rem',
+    paddingInline: '1.5rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-mobile-vertical': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(4, minmax(0, 1fr))',
+    columnGap: '1rem',
+    paddingInline: '1.25rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-xsm': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
+    columnGap: '1.25rem',
+    paddingInline: '1rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-sm': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(6, minmax(0, 1fr))',
+    columnGap: '0.75rem',
+    paddingInline: '1.5rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-md': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(8, minmax(0, 1fr))',
+    columnGap: '1rem',
+    paddingInline: '1.5rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.grid-lg': {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, minmax(0, 1fr))',
+    columnGap: '1rem',
+    paddingInline: '1.5rem',
+    alignItems: 'stretch',
+    justifyItems: 'stretch'
+  },
+  '.font-title-hero': {
+    fontFamily: 'Sora',
+    fontWeight: '700',
+    fontSize: '4.5rem',
+    letterSpacing: '-2.16px'
+  },
+  '.font-title-page': {
+    fontFamily: 'Sora',
+    fontWeight: '700',
+    fontSize: '3rem',
+    letterSpacing: '-0.96px'
+  },
+  '.font-subtitle': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '2rem',
+    letterSpacing: '0px'
+  },
+  '.font-heading': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '1.5rem',
+    letterSpacing: '0px'
+  },
+  '.font-subheading': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '1.25rem',
+    letterSpacing: '0px'
+  },
+  '.font-body-base': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '1rem',
+    letterSpacing: '0px'
+  },
+  '.font-body-strong': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '1rem',
+    letterSpacing: '0px'
+  },
+  '.font-body-link': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '1rem',
+    letterSpacing: '0px'
+  },
+  '.font-body-small': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '0.875rem',
+    letterSpacing: '0px'
+  },
+  '.font-body-small-strong': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '0.875rem',
+    letterSpacing: '0px'
+  },
+  '.text-body-base': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '1rem',
+    letterSpacing: '0px'
+  },
+  '.text-body-strong': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '0.875rem',
+    letterSpacing: '0px'
+  },
+  '.text-h1': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '2.25rem',
+    letterSpacing: '-1px'
+  },
+  '.text-h2': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '1.75rem',
+    letterSpacing: '-1px'
+  },
+  '.text-h3': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '1.25rem',
+    letterSpacing: '-0.5px'
+  },
+  '.text-h4': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '1rem',
+    letterSpacing: '0px'
+  },
+  '.text-body': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '0.875rem',
+    letterSpacing: '0px'
+  },
+  '.text-caption': {
+    fontFamily: 'Sora',
+    fontWeight: '400',
+    fontSize: '0.75rem',
+    letterSpacing: '0px'
+  },
+  '.text-caption-strong': {
+    fontFamily: 'Sora',
+    fontWeight: '600',
+    fontSize: '0.75rem',
+    letterSpacing: '0px'
+  }
+};
